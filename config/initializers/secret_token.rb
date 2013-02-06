71207cab033692d5475f99f50af9865cd71af522
Minagawa	Dai <kodomo.no.koro.ni.mita.sora@gmail.com>
2013-02-06 10:34:36 +0900

    [#43901263] プロジェクトの新規作成
    - rails new catalog -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = '5f6b22cbab052a42a30c7138a642d2318a498d6ec01d735d6f814eca576c335322166408be5b3c51d750cd66f90d14f37916a3f1c8a29e498f98b21aba659b66'
